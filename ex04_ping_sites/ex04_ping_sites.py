140740d21225fe1d6ad0880d12d40793f165c0fe
Pedro <46246172+peperone9@users.noreply.github.com>
2026-05-09 23:31:31 -0300

    Add files via upload

---
+++ ex04_ping_sites/ex04_ping_sites.py
@@ -0,0 +1,63 @@
+import subprocess
+import platform
+import multiprocessing
+
+def acessar(requisicao):
+	alvo: str = ""
+	buffer = requisicao.stdout.readline().decode("utf-8", errors="ignore")
+	while buffer != "":
+		if "avg" in buffer or "Average" in buffer:
+			alvo = buffer
+			return alvo
+		buffer = requisicao.stdout.readline().decode("utf-8", errors="ignore")
+	return "erro ao executar o comando"
+
+def media_linux(conteudo):
+	media = conteudo.split(" = ")
+	media = media[1].split("/")
+	media = media[1]
+	return media + "ms"
+def media_windows(conteudo):
+	media = conteudo.split(", ")
+	media = media[2].split(" = ")
+	media = media[1]
+	return media
+
+def ping(site, url):
+	comando = ["ping", "-4", "-c", "10", url]
+	if(platform.system() == "Linux"):
+		resposta = subprocess.Popen(comando, stdout=subprocess.PIPE)
+		conteudo = acessar(resposta)
+		tempo = media_linux(conteudo)
+		print(f"\n{site}\n{tempo}")
+	elif(platform.system() == "Windows"):
+		comando = ["ping", "-4", "-n", 10, url]
+		resposta = subprocess.Popen(comando, stdout=subprocess.PIPE)
+		conteudo = acessar(resposta)
+		tempo = media_windows(conteudo)
+		print(f"\n{site}\n{tempo}")
+	else:
+		print("Sistema operacional não compatível com a aplciacao")
+
+def main():
+	urls = [
+		"www.google.com",
+		"www.terra.com.br",
+		"www.uol.com.br"
+		]
+
+	sites= [
+		"Google",
+		"UOL",
+		"Terra"
+		]
+	parametros = [0] * 3 
+
+	for i in range(3):
+		parametros[i] = (sites[i], urls[i])
+		#ping(sites[i], urls[i]) #testa linear
+	with multiprocessing.Pool(processes=3) as p:
+		p.starmap(ping, parametros)
+
+if __name__ == "__main__":
+	main()
