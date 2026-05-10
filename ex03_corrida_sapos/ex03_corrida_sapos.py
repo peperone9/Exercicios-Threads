06405a07d3ad9c861fb33c2e7a4db0917c51f96a
Pedro <46246172+peperone9@users.noreply.github.com>
2026-05-10 20:48:29 -0300

    ex03 - corrida de sapos

--- ex03_corrida_sapos/ex03_corrida_sapos.py
+++ ex03_corrida_sapos/ex03_corrida_sapos.py
@@ -1,3 +1,10 @@
+# 3 - Fazer uma aplicação de uma corrida de sapos, com 5 Threads, cada Thread controlando 1 sapo. 
+# Deve haver um tamanho máximo para cada pulo do sapo (em centímetros) e a distância máxima para que os sapos percorram. 
+# A cada salto, um sapo pode dar um salto de 0 até o tamanho máximo do salto (valor aleatório entre 1 e 5 cm.). Após dar um salto,
+# a Thread, para cada sapo, deve mostrar no console, qual foi o tamanho do salto e quanto o sapo percorreu. Assim que o 
+# sapo percorrer a distância máxima, a Thread deve apresentar que o sapo chegou. 
+# Dica: O exercício deve ser resolvido todo em console, ou seja, como se estivesse sendo narrado.
+
 import multiprocessing
 import random
 import time
